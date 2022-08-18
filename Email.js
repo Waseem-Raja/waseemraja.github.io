23344a5c7f5363cfb3edb1e3472820d0d038f12c
Waseem-Raja <waseemrajamca44@gmail.com>
2022-08-18 21:18:48 +0530

    portfolio

---
+++ Email.js
@@ -0,0 +1,20 @@
+
+
+// script.js
+function SendMail() 
+{
+    var params =
+    {
+        from_name: document.getElementById("fullName").value,
+        email_id : document.getElementById("email_id").value,
+        message : document.getElementById("message").value
+    }
+    emailjs.send("service_fw5gb5v","template_rbal1mh",params).then(function(res){
+        alert("email sent sucessfully");
+         document.getElementById("fullName").value="";
+         document.getElementById("email_id").value="";
+         document.getElementById("message").value="";
+    })
+   
+}
+
